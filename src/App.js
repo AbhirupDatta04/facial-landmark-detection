e60738e7084765750eb72db2e897ce13c7f0002f
AbhirupDatta04 <66831444+AbhirupDatta04@users.noreply.github.com>
2021-07-14 21:31:11 +0530

    mesh

--- src/App.js
+++ src/App.js
@@ -3,12 +3,49 @@ import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/face-landmarks-detection";
 import Webcam from 'react-webcam';
+import { drawMesh } from "./utilities";
 
 
 function App() {
 
     const webcamRef=useRef('null');
     const canvasRef=useRef('null');
+
+    const runFacemesh = async () => {
+    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+    setInterval(() => {
+      detect(net);
+    }, 10);
+  };
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces({input:video});
+      console.log(face);
+
+      // Get canvas context
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(()=>{drawMesh(face, ctx)});
+    }
+  };
+
+  //useEffect(()=>{runFacemesh()}, []);
     
   return (
     <div className="App">
@@ -48,5 +85,3 @@ function App() {
 }
 
 export default App;
-
- 
